a077042e676485443a2fa21f064718e0c38859c4
saiteja1904 <reddysaiteja@gmail.com>
2020-11-25 00:16:12 +0800

    edited NotificationController

--- my-Starsplanner/src/stars/NotificationController.java
+++ my-Starsplanner/src/stars/NotificationController.java
@@ -1,6 +1,4 @@
 package stars;
-import java.util.HashMap;
-
 public class NotificationController {
     /**
      * default constructor
@@ -14,15 +12,8 @@ public class NotificationController {
      * @param recipient, name, indexNum, courseCode, keyMethod
      */
     public void notify(String recipient, String name, int indexNum, String courseCode, String keyMethod) {
-        //FileController fc = new FileController();
         NotificationFactory notifType = new NotificationFactory();
-        //HashMap<String, String> choice = student.getNotificationType();
-        //String keyMethod = choice.keySet().stream().findFirst().get();
-        //String recipient = choice.get(keyMethod);
         Notifications preferredNotifMethod = notifType.getNotifObj(keyMethod);
-        //String name = student.getName();
-        //int indexNum = index.getIndexNum();
-        //String courseCode = fc.getCourseByCode(index.getCourse()).getCourseCode();
         preferredNotifMethod.sendNotification(recipient, name, indexNum, courseCode);
     }
 }
